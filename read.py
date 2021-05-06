597f9799e44d8931f0eba96b87b9bf9a16bbc6bf
Dragos Velicanu <dragos@velicanu.com>
2021-05-05 22:19:32 -0400

    read WIP

--- read.py
+++ read.py
@@ -5,7 +5,12 @@ import logging
 from binascii import Error as BinasciiError
 from dateutil import parser
 import multiprocessing as mp
-
+import subprocess
+import click
+import json
+import hashlib
+import sqlite3
+import copy
 
 logging.basicConfig(
     format="%(asctime)s %(message)s",
@@ -28,7 +33,7 @@ def get_filename(payload):
 
 def get_new_filename(filename, folder, datestring):
     # strip timezone from datestring since it sometimes breaks parsing
-    date = parser.parse(datestring.split("+")[0]).date().isoformat()
+    date = parser.parse(datestring.split("+")[0].split("-")[0]).date().isoformat()
 
     suffix = 1
     new_filename = f"{date}-{filename}"
@@ -38,9 +43,9 @@ def get_new_filename(filename, folder, datestring):
     return os.path.join(folder, new_filename)
 
 
-def save_file(filename, attachment, date):
+def save_file(filename, attachment, date, message, part):
     new_filename = get_new_filename(filename, "attachments", date)
-    log.info(f"{new_filename}")
+    # log.info(f"{new_filename}")
 
     try:
         with open(new_filename, "wb") as fh:
@@ -51,27 +56,120 @@ def save_file(filename, attachment, date):
         with open(new_filename, "w") as fh:
             fh.write(attachment)
 
+    return new_filename
 
-def process_chunk(filename):
+
+def save_attachment(message, part):
+    if part.get_content_disposition() == "attachment" and get_filename(part):
+        return save_file(
+            part.get_filename(), part.get_payload(), message["date"], message, part
+        )
+
+
+def get_json(message, part, json_file):
+    if (
+        part["Content-Type"]
+        and "text" in part["Content-Type"]
+        and type(part.get_payload()) == str
+        and message["date"]
+    ):
+        output = {
+            "from_": message["from"],
+            "to_": message["to"],
+            "subject": message["subject"],
+            "date": parser.parse(
+                message["date"].split("+")[0].split("-")[0]
+            ).isoformat(),
+            "body": part.get_payload(),
+            # "attachment": "",
+        }
+        output["hash"] = hashlib.sha256(json.dumps(output).encode()).hexdigest()[:16]
+        return output
+
+
+def insert(batch, con):
+    first = batch[0]
+    qs = ["?" for v in first]
+    con.executemany(
+        f"""
+        INSERT INTO messages({",".join(first.keys())})
+        VALUES ({",".join(qs)})
+        """,
+        [tuple(v.values()) for v in batch],
+    )
+    con.commit()
+    log.info(f"Inserted {len(batch)} messages into database.")
+
+
+def process_chunk(filename, attachment, jsons, database):
     mbox = mailbox.mbox(filename)
+    chunk_number = filename.replace(".chunks/chunk_", "").replace(".txt", "")
+    os.makedirs("jsons", exist_ok=True)
+    json_file = open(f"jsons/mail_{chunk_number}.jsonl", "w")
+
+    con = sqlite3.connect("example.db")
+    insert_batch = []
+
     for message in mbox:
         for part in message.walk():
-            if (
-                # type(part) != str
-                part.get_content_disposition() == "attachment"
-                and get_filename(part)
-            ):
-                save_file(
-                    part.get_filename(),
-                    part.get_payload(),
-                    message["date"],
-                )
+            output_json = get_json(message, part, json_file)
+            if attachment:
+                saved_file = save_attachment(message, part)
+                if saved_file and message["message-id"]:
+                    print("a", message["subject"], message["message-id"])
+            if jsons and output_json:
+                if message["message-id"]:
+                    # if message["subject"] and "LNS" in message["subject"]:
+                    # breakpoint()
+                    print("j", message["subject"], message["message-id"], output_json)
+                json_file.write(json.dumps(output_json) + "\n")
+            if database and output_json:
+                trimmed = copy.deepcopy(output_json)
+                trimmed["body"] = output_json["body"][:255]
+                insert_batch.append(trimmed)
+
+    if insert_batch:
+        insert(insert_batch, con)
+    json_file.close()
+    con.close()
 
 
-def read():
+def split_mbox(mbox_file):
+    """
+    Split the mbox file into smaller chunks using awk for parallel processing.
+
+    If mbox file is already split, it does not re-split
+    """
+    os.makedirs(".chunks", exist_ok=True)
+    mbox_size = os.path.getsize(mbox_file)
+    chunk_size = sum(d.stat().st_size for d in os.scandir(".chunks") if d.is_file())
+
+    if abs(mbox_size - chunk_size) / mbox_size > 0.0001:
+        subprocess.run(["awk", "-f", "awk.txt", mbox_file], check=True)
+
+
+@click.command()
+@click.argument("mbox_file")
+@click.option("-a", "--attachments", is_flag=True)
+@click.option("-j", "--jsons", is_flag=True)
+@click.option("-d", "--database", is_flag=True)
+def read(mbox_file, attachments, jsons, database):
+    """
+    asdf
+    """
+
+    split_mbox(mbox_file)
+    with sqlite3.connect("example.db") as con:
+        con.execute(
+            """
+CREATE TABLE IF NOT EXISTS messages
+(from_ text, to_ text, subject text, date text, body text, hash text)
+            """
+        )
+
     chunks = [
-        os.path.join("chunks", c)
-        for c in os.listdir("chunks")
+        os.path.join(".chunks", c)
+        for c in os.listdir(".chunks")
         if c.startswith("chunk_")
     ]
 
@@ -79,9 +177,8 @@ def read():
     pool = mp.Pool(n_workers)
 
     for chunk in chunks:
-        # process_chunk(chunk)
-        pool.apply_async(process_chunk, args=(chunk,))
-
+        process_chunk(chunk, True, True, True)
+        # pool.apply_async(process_chunk, args=(chunk, True, True, True))
     pool.close()
     pool.join()
 
